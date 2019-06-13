58243d6026cc717aa76f5b447566e815f20d6988
rongfengliang <1141591465@qq.com>
2019-06-13 23:40:02 +0800

    add  minio bot3 demoo

---
+++ minioapp/app.py
@@ -0,0 +1,7 @@
+
+def main():
+    print("minio app start")
+
+if __name__ == '__main__':
+    main()
+    
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+setuptools.setup(
+    name="minioapp",
+    version="0.0.1",
+    author="dalongrong",
+    author_email="1141591465@qq.com",
+    description="minio app",
+    # install_requires=['boto3'],
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'minioapp=minioapp.app:main',
+        ],
+    }
+)
