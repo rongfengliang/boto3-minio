39d5a081f807a336e287ef3dca3254412053a94f
rongfengliang <1141591465@qq.com>
2019-06-16 09:36:11 +0800

    add basic get s3 bucket list

--- minioapp/app.py
+++ minioapp/app.py
@@ -1,7 +1,17 @@
 
+import boto3
+
+
 def main():
-    print("minio app start")
+    session = boto3.session.Session()
+    s3_client = session.client(
+        service_name='s3',
+        aws_access_key_id='dalongapp',
+        aws_secret_access_key='dalongapp',
+        endpoint_url='http://localhost:9000',
+    )
+    print(s3_client.list_buckets())
+
 
 if __name__ == '__main__':
     main()
-    
